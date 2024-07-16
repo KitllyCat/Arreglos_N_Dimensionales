9ca5c8cd4588620b0aebb19bf65d7c70754edf17
KitllyCat <cjchambem@unjbg.edu.pe>
2024-07-16 00:08:06 -0500

    Se añadio el ejercicio 08, espiral de una matriz

--- Ejercicio_08.cpp
+++ Ejercicio_08.cpp
@@ -6,69 +6,55 @@ using namespace std;
 
 const int N = 5;
 
-void recorridoEspiral(int matriz[5][5], int espiral[]){
+void recorridoEspiral(int matriz[5][5]) {
     int inicioFila = 0, finFila = N - 1;
     int inicioColumna = 0, finColumna = N - 1;
-    int index = 0;
 
-    while (inicioFila <= finFila && inicioColumna <= finColumna){
+    while (inicioFila <= finFila && inicioColumna <= finColumna) {
         for (int i = inicioColumna; i <= finColumna; ++i)
-            espiral[index++] = matriz[inicioFila][i];
+            cout << matriz[inicioFila][i] << " ";
         ++inicioFila;
 
         for (int i = inicioFila; i <= finFila; ++i)
-            espiral[index++] = matriz[i][finColumna];
+            cout << matriz[i][finColumna] << " ";
         --finColumna;
 
-        if (inicioFila <= finFila){
+        if (inicioFila <= finFila) {
             for (int i = finColumna; i >= inicioColumna; --i)
-                espiral[index++] = matriz[finFila][i];
+                cout << matriz[finFila][i] << " ";
             --finFila;
         }
 
-        if (inicioColumna <= finColumna){
+        if (inicioColumna <= finColumna) {
             for (int i = finFila; i >= inicioFila; --i)
-                espiral[index++] = matriz[i][inicioColumna];
+                cout << matriz[i][inicioColumna] << " ";
             ++inicioColumna;
         }
     }
 }
 
-void mostrarMatriz(int matriz[5][5]){
+void mostrarMatriz(int matriz[5][5]) {
     for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j){
+        for (int j = 0; j < 5; ++j) {
             cout << matriz[i][j] << " ";
         }
         cout << endl;
     }
 }
 
-void mostrarEspiral(int espiral[]) {
-    for (int i = 0; i < 25; ++i) {
-        cout << espiral[i] << " ";
-        if ((i + 1) % 5 == 0)
-            cout << endl;
-    }
-}
-
 int main() {
     int matriz[5][5] = {
-        {1, 9, 3, 7, 5},
-        {6, 2, 8, 4, 10},
-        {11, 18, 13, 16, 15},
-        {21, 12, 14, 19, 20},
-        {22, 23, 24, 25, 17}
+	{1, 9, 3, 7, 5},
+	{6, 2, 8, 4, 10},
+	{11, 18, 13, 16, 15},
+	{21, 12, 14, 19, 20},
+	{22, 23, 24, 25, 17}
     };
-
-    cout << "La matriz original es:" << endl;
+    cout<<"Matriz(A):"<<endl;
     mostrarMatriz(matriz);
-    cout << endl;
-
-    int espiral[25];
-    recorridoEspiral(matriz, espiral);
-
-    cout << "La matriz en forma de espiral es:" << endl;
-    mostrarEspiral(espiral);
+	cout<<endl;
+	cout<<"La matriz en forma de espiral es:"<<endl;
+    recorridoEspiral(matriz);
 
     return 0;
 }
